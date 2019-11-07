bf2d99457e199ee2d4eccd1e3a3050047a826f80
marquirj <juanmartinquiros@correo.ugr.es>
2019-11-07 15:21:10 +0100

    Prueba moda

--- analyzer/Conversacion.py
+++ analyzer/Conversacion.py
@@ -2,7 +2,7 @@ from grupo import Grupo
 
 
 class Analyzer:
-        
+
 
 	def __init__(self,grupos1=[]):
 	        self.grupos = grupos1
@@ -14,25 +14,21 @@ class Analyzer:
 	def getGrupos(self):
 		return self.grupos
 
-        def cuentaMensaje(self,mensajes):
-                return len(mensajes)
-        
-        def mensajeMasLargo(self,mensajes):
-                m = []
-                for mensaje in mensajes:
-                        if(len(mensaje)>len(m)):
-                                m = mensaje
-                return m, len(m)
-                        
+    def cuentaMensaje(self,mensajes):
+        return len(mensajes)
+
+    def mensajeMasLargo(self,mensajes):
+        m = []
+        for mensaje in mensajes:
+            if(len(mensaje)>len(m)):
+                m = mensaje
+        return m, len(m)
+
 	def grupoMasActivo(self):
-                longitud = 0
-     	        for grupo in self.grupos:
-     		        n=self.cuentaMensaje(grupo.getMensajes())
-     		        if n > longitud:
-     			        longitud=n
-     			        grupoDef = grupo
-     	        return grupoDef
-
-
-
-
+        longitud = 0
+        for grupo in self.grupos:
+            n=self.cuentaMensaje(grupo.getMensajes())
+            if n > longitud:
+    	        longitud=n
+    	        grupoDef = grupo
+        return grupoDef
